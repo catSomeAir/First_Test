0a681c2bdc415a3f26031390fe8c4f4b8cef0a30
TopYun <jmgenius683@gmail.com>
2022-10-25 22:14:37 +0900

    Update Ex01_Test.java

--- Ex01_Test.java
+++ Ex01_Test.java
@@ -34,6 +34,7 @@ public class Ex01_Test {
 //10월 22일 check
 //10월 22일 check
 //10월 22일 check
+//10월 25일 check
 	//오라클에 연결되는지 테스트 메소드 
 //	public boolean connTest() {	//url, user, password, Ojdbc Class ( 동적 로딩 = 자주안씀)
 //		
